2e1f2d8009626c66fc48a24edacd7c3f29dcbc82
Brahim Berriche <brahim.berriche@gmail.com>
2015-10-06 17:33:25 +0200

    Correction des bugs pour lire tous path

--- Lecture svg - Qt/main.cpp
+++ Lecture svg - Qt/main.cpp
@@ -16,15 +16,7 @@ int main(int argc, char *argv[])
 	QDomDocument *dom = new QDomDocument("MonDom");
 
 
-<<<<<<< HEAD
     QFile svg_doc("/Users/Mustapha/Qt/Projets/lireSvg/pirate.svg");
-=======
-	//QFile svg_doc("/Users/Mustapha/Qt/Projets/lireSvg/package7.svg");
-
-	//QFile svg_doc("C:/Users/Thomas/Desktop/IMERIR/3A/Projet Robotique - Kuka/IMERIR-Kuka-master/SVG/pirate.svg");
-	QFile svg_doc("C:/Users/Thomas/Desktop/IMERIR/3A/Projet Robotique - Kuka/IMERIR-Kuka-master/SVG/line.svg");
-	//QFile svg_doc("C:/Users/Thomas/Desktop/IMERIR/3A/Projet Robotique - Kuka/IMERIR-Kuka-master/SVG/city21.svg");
->>>>>>> afc1a39c9f20f25d3fa3fe8859749f332ef5029b
 
 
 	if (!svg_doc.open(QIODevice::ReadOnly)) {
@@ -34,12 +26,23 @@ int main(int argc, char *argv[])
 
 	dom->setContent(&svg_doc);
 
+
 	QDomElement dom_element = dom->documentElement();
 	QDomNode node = dom_element.firstChild(); //balise d
 	node = node.firstChild();
 	QString ligne= "";
 
-<<<<<<< HEAD
+    qDebug() << dom->toString();
+
+      QRegExp rxe("(width=\"\\d.+(pt)\").+(height=\"\\d.+(pt)\")");
+      int pos1 = 0;
+      if((pos1 = rxe.indexIn(dom->toString(), pos1)) != -1)
+      {
+        qDebug() <<"ee"+ (QString)rxe.cap(1);
+        qDebug() <<"ee"+ (QString)rxe.cap(3);
+      }
+
+
     /***/
         QRegExp rx("(<g.+\n.+)\>");
 
@@ -69,69 +72,38 @@ int main(int argc, char *argv[])
             }
          }
         /***/
-=======
-	/***/
-		QRegExp rx("(<g.+\n.+)\>");
 
-		 QStringList list;
-		 int pos = 0;
-		 QString translate_property="";
-		 QString scale_property="";
-		 if((pos = rx.indexIn(dom->toString(), pos)) != -1)
-		 {
-			QString nn=(QString)rx.cap(1);
-			QStringList charList =nn.split(' ');
-			QString chaineq="";
-			QRegExp rxproperties("(\\(|\\,|\\))"); //RegEx for ' ' or ',' or '.' or ':' or '\t'
-
-			int nb = charList.count(); //compter le nombre d'élément dans la liste
-			for(int j=0; j < nb; j++){ //faire une boucle pour parcourir la liste
-			QString chaine =charList.at(j).toLatin1().replace("\n"," ");
-				if(chaine.contains("translate") >= 1)
-				{
-					QStringList query = chaine.split(rxproperties);
-					translate_property+="{translate:x:"+query.at(1)+",y:"+query.at(2)+"}";
-				}else if(chaine.contains("scale") >= 1)
-				{
-					QStringList query = chaine.split(rxproperties);
-					scale_property+="{scale:x:"+query.at(1)+",y:"+query.at(2)+"}";
-				}
-			}
-		 }
-		/***/
->>>>>>> afc1a39c9f20f25d3fa3fe8859749f332ef5029b
-
+    qint32 index=1 ;
+QStringList charList ;
 	while(!node.isNull()){ //vérifier si un noeud Path est dans le fichier
 
-		QStringList charList = node.attributes().item(0).toAttr().value().split(' ');
 
-<<<<<<< HEAD
+         charList += node.attributes().item(0).toAttr().value().split(' ');
+
         ligne = "";
-=======
-
-		ligne = "";
-
->>>>>>> afc1a39c9f20f25d3fa3fe8859749f332ef5029b
+        QString toto="";
 		int nb = charList.count(); //compter le nombre d'élément dans la liste
 		for(int j=0; j < nb; j++){ //faire une boucle pour parcourir la liste
-			ligne+=" "+charList.at(j).toLatin1().replace("\n"," ");
+            toto="";
+            toto+=" "+charList.at(j).toLatin1();
+            //qDebug() << toto;
+            ligne+=toto;
 		}
 
+        ligne=ligne.replace("\n"," ");
+        ligne=ligne.replace("\t","");
+        ligne=ligne.replace("\r","");
+        ligne=ligne.replace("l"," l");
+        ligne=ligne.replace("c"," c");
+        ligne=ligne.replace("L"," L");
+        ligne=ligne.replace("C"," C");
+        ligne=ligne.replace("-"," -");
+        ligne=ligne.replace("[,","[");
+        ligne=ligne.replace(",-","-");
+        ligne=ligne.replace(","," ");
+
+        qDebug() << ligne;
 		// Création d'une liste avec chaque paramètre
-        ligne.replace("\n","");
-        ligne.replace("\t","");
-        ligne.replace("\r","");
-        ligne.replace("l"," l");
-        ligne.replace("c"," c");
-        ligne.replace("L"," L");
-        ligne.replace("C"," C");
-        ligne.replace("-"," -");
-        ligne.replace("[,","[");
-        ligne.replace(",-","-");
-        ligne.replace(","," ");
-
-
-
 		QStringList query = ligne.split(" ");
 
 
@@ -155,6 +127,7 @@ int main(int argc, char *argv[])
 				   else if(str.startsWith("m"))
 				   {
                        jsonelement+="],m:["+QString("%1").arg(str.right(str.size()-1));
+                       jsonelement+=",";
 				   }
 				   else if(str.startsWith("c"))
 				   {
@@ -198,11 +171,11 @@ int main(int argc, char *argv[])
 
 
 
-			   }
+               }
 
 		}
 
-		json=json.replace("\n","");
+       json=json.replace("\n","");
         json=json.replace("\r","");
         json=json.replace("\t","");
 		json=json.replace(",]","]");
@@ -212,8 +185,8 @@ int main(int argc, char *argv[])
         json=json.replace("C","\"C\"");
 		json=json.replace("c","\"c\"");
 		json=json.replace("l","\"l\"");
-		json=json.replace("m","\"m\"");
-		json=json.replace("\n\"","");
+        json=json.replace("m","\"m\":");
+        json=json.replace("\n\"","");
         json=json.replace("}{",",");
         json=json.replace("],{","],");
         json=json.replace("]}]","]");
@@ -221,20 +194,16 @@ int main(int argc, char *argv[])
         json=json.replace("]},","],");
         json=json.replace(",\"M\"","],\"M\"");
         json=json.replace("]]","]");
+        json=json.replace(":\":",":\"");
+        json=json.replace("::",":");
+        json=json.replace(":[,","[");
+        json=json.replace("\"m\"[","\"m\":[");
 
 
-<<<<<<< HEAD
-=======
-		/*if(translate_property!="")
-		{
-			json= translate_property;
-		} */
->>>>>>> afc1a39c9f20f25d3fa3fe8859749f332ef5029b
 
-		node = node.nextSibling(); //Aller au Path suivant
+        node = node.nextSiblingElement("path"); //Aller au Path suivant
 
 	}
-<<<<<<< HEAD
     if(translate_property!="")
     {
         json= translate_property+","+json;
@@ -250,19 +219,6 @@ int main(int argc, char *argv[])
         json="{"+json;
     }
     qDebug() <<json +"\n\r";
-=======
-	if(translate_property!="")
-	{
-		json= translate_property+","+json;
-	}
-
-	if(scale_property!="")
-	{
-		json= scale_property+","+json;
-	}
-	//json= translate_property+","+scale_property+","+json;
-	qDebug() <<json +"\n\r";
->>>>>>> afc1a39c9f20f25d3fa3fe8859749f332ef5029b
 
 	return 0;
 }
